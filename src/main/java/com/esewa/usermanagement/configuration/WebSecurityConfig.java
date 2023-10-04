e7ded45e032b20cbaddf904a7ead530d9acd66ec
Aayushchy <caayush96@gmail.com>
2023-10-04 13:32:58 +0545

    Fixed Servlet Mapping Issues

--- src/main/java/com/esewa/usermanagement/configuration/WebSecurityConfig.java
+++ src/main/java/com/esewa/usermanagement/configuration/WebSecurityConfig.java
@@ -4,17 +4,20 @@ import com.esewa.usermanagement.jwt.JwtAuthenticationEntryPoint;
 import com.esewa.usermanagement.jwt.JwtAuthenticationFilter;
 import com.esewa.usermanagement.service.impl.CustomUserDetailServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 @EnableWebSecurity
@@ -38,7 +41,13 @@ public class WebSecurityConfig {
         return configuration.getAuthenticationManager();
     }
 
-    @Bean
+    /*@Bean
+    public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> enableDefaultServlet() {
+        return factory -> factory.setRegisterDefaultServlet(true);
+    }
+*/
+
+   /* @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         return http
                 .csrf(AbstractHttpConfigurer::disable)
@@ -54,7 +63,29 @@ public class WebSecurityConfig {
                 .build();
 
     }
+*/
+//    @Bean
+//    MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
+//        return new MvcRequestMatcher.Builder(introspector);
+//    }
 
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
+        MvcRequestMatcher.Builder mvc = new MvcRequestMatcher.Builder(introspector);
+        return http
+                .authorizeHttpRequests((requests) -> requests
+                                .requestMatchers(mvc.pattern("/homepage/**")).permitAll()
+                                .requestMatchers(mvc.pattern("/admin/**")).hasRole("ADMIN")
+                                .requestMatchers(mvc.pattern("/user/**")).hasAnyRole("ADMIN", "USER")
+                                .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
